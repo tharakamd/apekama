6ce0a94a5ec4abdede3d8568654d9c1dcafdaafa
npamudika <rpnpamudika@gmail.com>
2015-03-15 15:46:26 +0530

    Image_Slider updated

--- includes/image_slider.php
+++ includes/image_slider.php
@@ -1,1 +1,31 @@
-<p>Image Slider</p>
+
+
+<div class="container">
+        <div class="carousel-slide" data-ride="carousel" id="carousel-ex">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-ex" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-ex" data-slide-to="1"></li>
+                <li data-target="#carousel-ex" data-slide-to="2"></li>
+                <li data-target="#carousel-ex" data-slide-to="3"></li>
+                <li data-target="#carousel-ex" data-slide-to="4"></li>
+            </ol>
+            
+            <div class="carousel-inner">
+                <div class="item active">
+                    <span><img src="images/12N.jpg" class="slides"></span>
+                </div>
+                <div class="item">
+                    <span><img src="images/13N.jpg" class="slides"></span>
+                </div>
+                <div class="item">
+                    <span><img src="images/17N.jpg" class="slides"></span>
+                </div>
+                <div class="item">
+                    <span><img src="images/15N.jpg" class="slides"></span>
+                </div> 
+                <div class="item">
+                    <span><img src="images/16N.jpg" class="slides"></span>
+                </div>
+            </div>
+        </div>
+    </div> 
