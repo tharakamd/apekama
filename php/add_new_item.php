196bc40569a0851d37d6fb851567a192571cf52c
= <=>
2015-04-18 11:46:22 +0530

    modificationof item window

--- php/add_new_item.php
+++ php/add_new_item.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "Testdatabase";
+$dbname = "apekama_db";
 
 $category = $_POST["category"];
 $price = $_POST["price"];
@@ -18,32 +18,8 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-// Create database
-//        $sql = "CREATE DATABASE Testdatabase";
-//        if (mysqli_query($conn, $sql)) {
-//            echo "Database created successfully";
-//        } else {
-//            echo "Error creating database: " . mysqli_error($conn);
-//        }
-// sql to create table
-//        $sql = "CREATE TABLE itemdata (                
-//                category varchar(20),
-//                price decimal(10,0),
-//                warrenty varchar(8),
-//                quantity varchar(5),
-//                item_description varchar(8),
-//                image blob
-//                )";
-//
-//        if ($conn->query($sql) === TRUE) {
-//            echo "Table item data created successfully";
-//        } else {
-//            echo "Error creating table: " . $conn->error;
-//        }
-
-$sql = "INSERT INTO itemdata (category,price,warrenty,quantity,item_description,image)
-            VALUES ('" . $category . "', '" . $price . "','" . $warrenty . "','" . $quantitiy . "','" . $description . "','" . $inputfile . "')";
+$sql = "INSERT INTO items (item_id,price,seller_id,description,warrenty,image,available,sold,added_date,category)
+            VALUES ('" . 12345 . "', '" . $price . "','" . 234 . "','" . $description . "','" . $warrenty . "','" . $inputfile . "','" . 4 . "','" . 5 . "','" .  date("Y.m.d") . "','" .$category. "')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
